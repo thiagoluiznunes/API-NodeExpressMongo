037d30cd8fea0eac7e4d37e27ab87c36fc8f1a47
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-08-06 00:29:31 -0300

    refactoring item schema

--- myFolder/config/database.js
+++ myFolder/config/database.js
@@ -1,2 +1,4 @@
 const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://user:password@localhost:2727/myDataBase',{useMongoClient: true})
+module.exports = mongoose.connect('mongodb://user:password@localhost:2727/myDataBase',{
+  useMongoClient: true
+})
